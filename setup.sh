441553628e59838f19d9d8b7149d04c364e0a2d8
William Horvath <william@horvath.blog>
2024-09-18 05:51:52 -0700

    make everything way more complicated while not really achieving much

--- setup.sh
+++ setup.sh
@@ -14,17 +14,18 @@ umu_launcherurl=https://github.com/Open-Wine-Components/umu-launcher.git
 # Do everything
 ##############################################
 _main() {
-    { _dirsetup && _envsetup ; } || _failure "Failed preparing script environment."
+    _script_realpath=$(realpath "$(dirname "$0")")
 
-    if [ "${_do_build}" = "true" ]; then {
-        _sources &&
-        _patch proton wine &&
-        _build
-        } || _failure "Build failed."
-    fi
+    _envsetup || _failure "Failed preparing build environment."
+    _dirsetup_initial || _failure "Failed initial directory setup."
 
-    if [ "${_do_install}" = "true" ]; then
-        _install || _failure "Install failed."
+    if [ "${_do_build}" = "true" ]; then
+        _sources || _failure "Failed to prepare sources."
+        _patch proton wine || _failure "Failed to apply patches."
+        _build || _failure "Build failed."
+        if [ "${_do_install}" = "true" ]; then
+            _install || _failure "Install failed."
+        fi
     fi
 
     _message "Script finished."
@@ -37,25 +38,15 @@ _parse_args() {
     # will exit early here if help specified
     if [[ "${*}" =~ .*help.* ]]; then _help; fi
 
-    # messed up way to build&install if either "build install" is specified or neither are specified
-    if     [[ "${*}" =~ ^build ]] && [[ "${*}" =~ "install" ]] ||
-       ! { [[ "${*}" =~ ^build ]] || [[ "${*}" =~ "install" ]] ; }; then
+    if [[ "${*}" =~ "buildonly" ]]; then
+        _do_build=true
+        _message "Will only be building Proton without installing it."
+    else
         _do_build=true
         _do_install=true
         _message "Will build Proton and also install it to the Steam compatibility tools directory."
-    elif [[ "${*}" =~ ^build ]]; then
-        _do_build=true
-        _message "Will only be building Proton without installing it."
-    elif [[ "${*}" =~ "install" ]]; then
-        _do_install=true
-        _message "Will only install the already built files to the Steam compatibility tools directory."
-        return
     fi
 
-    if [[ "${*}" =~ "reclone" ]]; then
-        _do_reclone=true
-        _message "Will reclone sources before bulding."
-    fi
     if [[ "${*}" =~ "no-bundle-umu" ]]; then
         _do_bundle_umu=false
         _message "Won't bundle umu-run with Proton."
@@ -73,59 +64,27 @@ _parse_args() {
     fi
 }
 ##############################################
-# Download sources
-##############################################
-_sources() {
-    if [ "${_do_reclone}" = "true" ]; then rm -rf "${srcdir}"; fi
-
-    if ! { [ -d "${srcdir}" ] && [ -f "${srcdir}"/Makefile ] ; }; then
-        git clone --depth 1 --recurse-submodules --shallow-submodules "${protonurl}" "${srcdir}" -b "${protontag}" ||
-            _failure "Couldn't clone your chosen Proton repo at the tag."
-    fi
-
-    git -C "${srcdir}" reset --hard --recurse-submodules HEAD ||
-        _failure "Couldn't reset the Proton sources to their original state."
-
-    # Keep protonfixes up-to-date, since we don't pin it to a specific version 
-    rm -rf "${srcdir}"/protonfixes "${scriptdir}"/protonfixes
-    git clone --depth 1 --recurse-submodules --shallow-submodules "${umu_protonfixesurl}" "${scriptdir}"/protonfixes ||
-        _failure "Couldn't add the required umu-protonfixes repo."
-    cp -r "${scriptdir}"/protonfixes "${srcdir}"/protonfixes
-
-    if [ "${_do_bundle_umu}" = "true" ]; then
-        # Same with umu-launcher, if we want that
-        rm -rf "${scriptdir}"/umu-launcher
-        git clone --depth 1 --recurse-submodules --shallow-submodules "${umu_launcherurl}" "${scriptdir}"/umu-launcher ||
-            _failure "Couldn't add the umu-launcher repo you wanted."
-    fi
-
-    cd "${scriptdir}"
-    _message "Sources are ready."
-}
-##############################################
 # Directories
 ##############################################
-_dirsetup() {
-    scriptdir="$(pwd)" && export scriptdir
-
-    srcdir="${scriptdir}"/proton && export srcdir
-    builddir="${scriptdir}"/build && export builddir
+_dirsetup_initial() {
+    scriptdir="${_script_realpath}" && export scriptdir
+    srcdir="${scriptdir}/proton" && export srcdir
+    builddir="${scriptdir}/build" && export builddir
     if [ ! -d "${builddir}" ]; then mkdir "${builddir}"; fi
 
-    patchdir="${scriptdir}"/patches && export patchdir
+    build_out_dir="${scriptdir}/build_tarballs"
+    if [ ! -d "${build_out_dir}" ]; then mkdir "${build_out_dir}"; fi
 
-    # Currently hardcoded in the Makefile anyways, but we can switch to other manual installation methods easily
-    # A tar.xz file is dropped in the build/ directory as well
-    installdir="${HOME}"/.steam/root/compatibilitytools.d/"${pkgname}" && export installdir
+    patchdir="${scriptdir}/patches" && export patchdir
 }
 ##############################################
-# Env
+# Environment
 ##############################################
 _envsetup() {
     # For wineprefix setup during build
     export WINEESYNC=0
     export WINEFSYNC=0
-    unset DISPLAY
+    export DISPLAY=
 
     CPUs="$(nproc)" && export CPUs
     export MAKEFLAGS="-j$CPUs"
@@ -143,8 +102,7 @@ _patch() {
     for subdir in "${@}"; do
         # "hack" to support patches that are rooted in wine
         if [ "${subdir}" = "wine" ]; then 
-            cd "${srcdir}/wine" &&
-            git clean -xdf
+            cd "${srcdir}/wine"
         fi
 
         mapfile -t patchlist < <(find "${patchdir}/${subdir}" -type f -regex ".*\.patch" | LC_ALL=C sort -f)
@@ -181,18 +139,20 @@ _build() {
         make clean
     fi
 
-    make -j1 redist &&
+    make -j1 redist || _failure "Build failed."
+
     if [ "${_do_bundle_umu}" = "true" ]; then 
         cd "${scriptdir}"/umu-launcher && ./configure.sh --user-install && mkdir pkg && make DESTDIR=pkg install && cd "${builddir}"
         rm -rf "${buildname}"/umu-run
         shopt -s globstar
-    	cp -a "${scriptdir}"/umu-launcher/pkg/**/.local/bin/umu-run "${buildname}"/umu-run
+        cp -a "${scriptdir}"/umu-launcher/pkg/**/.local/bin/umu-run "${buildname}"/umu-run
         shopt -u globstar
-    fi &&
+    fi
+
     _message "Creating archive: ${pkgname}.tar.xz" &&
-    XZ_OPT="-9 -T0" tar -Jcf "${scriptdir}"/"${pkgname}".tar.xz --numeric-owner --owner=0 --group=0 --null "${buildname}" &&
-    sha512sum "${scriptdir}"/"${pkgname}".tar.xz > "${scriptdir}"/"${pkgname}".sha512sum &&
-    _message "${pkgname}.tar.xz is now ready in the current directory"
+    XZ_OPT="-9 -T0" tar -Jcf "${build_out_dir}"/"${pkgname}".tar.xz --numeric-owner --owner=0 --group=0 --null "${buildname}" &&
+    sha512sum "${build_out_dir}"/"${pkgname}".tar.xz > "${build_out_dir}"/"${pkgname}".sha512sum &&
+    _message "${pkgname}.tar.xz is now ready in the build_tarballs directory"
 }
 ##############################################
 # Install
@@ -206,11 +166,113 @@ _install() {
     _message "Along with the archive in the current directory"
 }
 ##############################################
+# Source preparation
+##############################################
+_sources() {
+    cd "${scriptdir}" || _failure "Couldn't change to script directory."
+
+    _repo_updater "proton" "${protonurl}" "${protontag}"
+
+    _repo_updater "protonfixes" "${umu_protonfixesurl}"
+
+    # Clean protonfixes from Proton and copy the new one
+    if [ -d "proton/protonfixes" ]; then rm -rf "proton/protonfixes"; fi
+    cp -r "protonfixes" "proton/protonfixes"
+
+    if [ "${_do_bundle_umu}" = "true" ]; then
+        _repo_updater "umu-launcher" "${umu_launcherurl}"
+    fi
+
+    _message "Sources are ready."
+}
+_repo_updater() {
+    local repo_path="$1"
+    local repo_url="$2"
+    local specific_ref="$3"
+
+    _message "Ensuring ${repo_path} is up-to-date."
+
+    if [ ! -d "${repo_path}" ]; then
+        _message "Cloning ${repo_path}."
+        git clone --depth 1 "${repo_url}" "${repo_path}" ||
+            _failure "Couldn't clone the ${repo_path} repository."
+        local is_new_clone=true
+    fi
+
+    cd "${repo_path}" || _failure "Couldn't change directory to ${repo_path}."
+
+    git fetch --depth 1 origin
+
+    local target_ref="${specific_ref:-origin/HEAD}"
+
+    # Check if the repository needs to be updated or cleaned
+    if [ "${is_new_clone}" = "true" ] || [ -n "$(git status --porcelain)" ] || [ "$(git rev-parse HEAD)" != "$(git rev-parse ${target_ref})" ]; then
+        _message "The ${repo_path} repository will be set to a clean state at ${target_ref}."
+
+        # Reset and clean the main repository
+        git reset --hard
+        git clean -ffdx
+
+        if [ -n "${specific_ref}" ]; then
+            git fetch --depth 1 origin "${specific_ref}:refs/remotes/origin/${specific_ref}" || true
+
+            # Tag case
+            if git rev-parse "refs/tags/${specific_ref}" >/dev/null 2>&1; then
+                git checkout -f "${specific_ref}" &&
+                _message "Checked out ${repo_path} at tag ${specific_ref}."
+            else # Branch case
+                git checkout -B "${specific_ref}" "origin/${target_ref}" &&
+                _message "Checked out ${repo_path} at branch ${specific_ref} targeting origin/${target_ref}."
+            fi
+        else
+            # Otherwise just reset to origin/HEAD
+            git reset --hard origin/HEAD
+        fi
+
+        # Keep submodules updated
+        if [ -f ".gitmodules" ]; then
+            _message "Updating submodules for ${repo_path}."
+            git submodule update --init --depth 1 --recursive
+            # shellcheck disable=SC2016
+            git submodule foreach --recursive '
+                if [ -n "$(git status --porcelain)" ]; then
+                    git reset --hard
+                    git clean -ffdx
+                fi
+            ' 2>/dev/null
+        fi
+
+        _message "Cleaned files from ${repo_path}"
+    else
+        _message "The ${repo_path} repository is already up-to-date and clean."
+    fi
+
+    # Delete any old tags we made
+    git tag -l "local-*" | xargs -r git tag -d
+
+    # Create a new "fake" tag at the current position with a timestamp, so that proton/protonfixes/umu-launcher is happy when versioning the build
+    local timestamp=$(date +%Y%m%d%H%M%S)
+    local current_branch=$(git rev-parse --abbrev-ref HEAD)
+    local local_tag="local-${current_branch}-${timestamp}"
+    local commit_hash=$(git rev-parse HEAD)
+    git tag -a -f "${local_tag}" -m "Local build tag for ${current_branch} at ${timestamp}" "${commit_hash}"
+    _message "Created temporary tag ${local_tag} for ${repo_path} to use in versioning."
+
+    cd "${scriptdir}" || _failure "Couldn't change directory back to script's directory (somehow.)"
+}
+##############################################
 # Log messages
 ##############################################
 _message() {
     if [ -n "$*" ]; then
         echo -e '\033[1;34m'"Message:\033[0m $*"
+    else
+        echo ""
+    fi
+}
+_warning() {
+    if [ -n "$*" ]; then
+        echo -e '\033[1;33m'"Message:\033[0m $*"
     else
         echo ""
     fi
@@ -221,14 +283,12 @@ _failure() {
     exit 1
 }
 _help() {
-    _message "./setup.sh [help] [reclone] [build (cleanbuild)] [bundle-umu] [install]"
+    _message "./setup.sh [help] [buildonly] [cleanbuild] [bundle-umu]"
     echo ""
     _message "No arguments grabs sources, patches, builds, and installs"
-    _message "Adding 'build' will only build without installing to the steam compatibility tools directory"
+    _message "  Adding 'buildonly' will only build without installing to the steam compatibility tools directory"
     _message "  Adding 'cleanbuild' just runs 'make clean' in the build directory before 'make'"
-    _message "  Adding 'no-bundle-umu' WON'T build the latest umu-launcher from master and place 'umu-run' it in Proton's toplevel directory"
-    _message "Adding 'install' will only install to the steam compatibility tools directory without rebuilding"
-    _message "Adding 'reclone' redownloads sources, use this if your sources are outdated"
+    _message "  Adding 'no-bundle-umu' WON'T build the latest umu-launcher from master and place 'umu-run' in Proton's toplevel directory"
 
     exit 0
 }
